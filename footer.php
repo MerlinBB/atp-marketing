ffec802dbbba8646a4433a2b869cc5109ce4c945
Merlin Mason <info@merlinmason.co.uk>
2013-09-23 17:06:00 +0100

    add in parallax.js to bower config and footer

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
-<footer class="pagefooter">
+<footer class="pagefooter text-center">
 	<p class="copyright">&copy; <?php bloginfo( 'name' ); ?>, <?php echo date("Y"); ?>. All rights reserved.</p>
 </footer>
 
 </div>
 
 <?php wp_footer(); ?>
+<script src="<?php bloginfo("template_url"); ?>/bower_components/parallax/source/jquery.parallax.js"></script>
 <script src="<?php bloginfo("template_url"); ?>/js/project.js"></script>
 </body>
 </html>
