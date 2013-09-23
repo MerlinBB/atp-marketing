0d90a04a0be238ef91e2d2b9e57972d5d051ae7e
Merlin Mason <info@merlinmason.co.uk>
2013-09-23 14:05:52 +0100

    toggle state of header based on scroll position

--- js/project.js
+++ js/project.js
@@ -1,3 +1,12 @@
 $(function() {
-	console.log("jQuery is running!");
+    
+    // switch header state based on scroll
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 150) { 
+            $(".page-header").addClass("compressed"); 
+        } else { 
+            $(".page-header").removeClass("compressed"); 
+        }
+    });
+
 });
