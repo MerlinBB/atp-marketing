4c18ef3612fa9bbdc77ad78b070ebd072695f0da
Merlin Mason <info@merlinmason.co.uk>
2013-09-28 19:27:38 +0100

    adjust routing logic to use posts for case-studies section

--- _case-studies.php
+++ _case-studies.php
@@ -1,6 +1,4 @@
 <?php get_header(); ?>
-
-	<h1><span>You're looking at:</span><?php printf( __( ' %s' ), '' . single_cat_title( '', false ) . '' ); ?></h1>
 	
 	<?php while ( have_posts() ) : the_post(); ?>
 		<article>
@@ -10,7 +8,7 @@
 	<?php endwhile; ?>
 	
 	<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-		<nav id="postnav" class="group">
+		<nav class="postnav" class="group">
 			<span class="prev"><?php previous_posts_link( __( '&#9756; More Recent Posts') ); ?></span>
 			<span class="next"><?php next_posts_link( __( 'Older Posts &#9758;') ); ?></span>
 		</nav>
