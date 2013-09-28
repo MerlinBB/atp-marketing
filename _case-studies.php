d782f0cb03c11f5db744d2bbe15f15dcdc930068
Merlin Mason <info@merlinmason.co.uk>
2013-09-28 21:00:21 +0100

    flesh out content for case studies

--- _case-studies.php
+++ _case-studies.php
@@ -1,10 +1,65 @@
 <?php get_header(); ?>
 	
 	<?php while ( have_posts() ) : the_post(); ?>
-		<article>
-			<h1><?php the_title(); ?></h1>
-			<?php the_content(); ?>
+	<div class="slidecontainer">
+		<article class="case-study slider">
+			<div class="row">
+				<div class="col-6">
+					<div class="row">
+						<div class="col-r-6">
+							<img src="http://placehold.it/453x300" alt="" class="scale">
+						</div>
+						<div class="col-r-6">
+							<h1 class="heading"><?php the_title(); ?></h1>
+							<h2 class="subheading">Subtitle</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, saepe, iusto consectetur perspiciatis repellat cum ratione culpa labore accusantium reprehenderit molestiae iste et nam nisi vel dolorum autem id architecto odio libero totam impedit quas! Provident, ipsa, nisi, fugit, dolores pariatur ipsum sunt hic repellendus odio quae odit quos quasi.</p>
+							<button class="moveslideshow" data-destination="1">Read More</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="row">
+						<div class="col-r-6">
+							<h1 class="heading"><?php the_title(); ?></h1>
+							<h2 class="subheading">Subtitle</h2>
+							<div class="row">
+								<div class="col-6">
+									<img src="http://placehold.it/209x209" alt="" class="scale img-circle mb-1x">
+									<h3 class="subheading">Feature title</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt quisquam iusto illum dolor accusantium quasi accusamus repellendus in dicta ipsa!</p>
+								</div>
+								<div class="col-6">
+									<img src="http://placehold.it/209x209" alt="" class="scale img-circle mb-1x">
+									<h3 class="subheading">Feature title</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, quidem, expedita nisi magnam tempora iste voluptatem cum maxime vero repellendus.</p>
+									<button class="moveslideshow" data-destination="0">Back</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-r-6">
+							<div class="popup">
+								<h3 class="subheading">Features</h3>
+								<ul class="list-features">
+									<li>
+										<h4 class="feature-title">Feature Title</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod consequuntur numquam nobis quis possimus quae.</p>
+									</li>
+									<li>
+										<h4 class="feature-title">Feature Title</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod consequuntur numquam nobis quis possimus quae.</p>
+									</li>
+									<li>
+										<h4 class="feature-title">Feature Title</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod consequuntur numquam nobis quis possimus quae.</p>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</article>
+	</div>
 	<?php endwhile; ?>
 	
 	<?php if (  $wp_query->max_num_pages > 1 ) : ?>
