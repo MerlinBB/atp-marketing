38e7c776f9ef44b25c3448643f3378c9b54b95f9
Merlin Mason <info@merlinmason.co.uk>
2013-09-28 21:00:46 +0100

    tidy js structure and build basic slider

--- js/project.js
+++ js/project.js
@@ -1,18 +1,40 @@
-$(function() {
-    
-    $("#welcome-scene").parallax();
+var atp = {
 
-    // switch header state based on scroll
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 150) { 
-            $(".page-header").addClass("compressed"); 
-        } else { 
-            $(".page-header").removeClass("compressed"); 
-        }
-    });
+    init: function () {
+        this.bindUIActions();
+        $("#welcome-scene").parallax();
+    },
 
-    $(".hamburger").click(function () {
-    	$("nav.menu").slideToggle("fast");
-    })
+    bindUIActions: function () {
 
+        $(".moveslideshow").on("click", function (e) {
+            atp.slider(e);
+        });
+
+        $(".hamburger").click(function () {
+            $("nav.menu").slideToggle("fast");
+        });
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 150) { 
+                $(".page-header").addClass("compressed"); 
+            } else { 
+                $(".page-header").removeClass("compressed"); 
+            }
+        });
+
+    },
+
+    slider: function (e) {
+        var destination = parseInt($(e.currentTarget).data("destination"));
+        var margin = -Math.abs(destination) * 100 + "%";
+
+        $(e.currentTarget).closest(".slider").css({ marginLeft: margin });
+    }
+
+};
+
+// DOM Ready
+$(function () {
+    atp.init();
 });
