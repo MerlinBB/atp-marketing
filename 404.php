608bc22e788856ad229f828a6c2619b786fc1436
Merlin Mason <info@merlinmason.co.uk>
2013-10-09 22:50:45 +0100

    tidy 404 template

--- 404.php
+++ 404.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 
 <section class="fourohfour">
-	<h1>Dang, we're lost!</h1>
-	<p>Sorry, we're lost this</p>
-	<p>Why don't you have a search?</p>
-	<?php get_search_form(); ?>
+	<h1>404 - Dang!</h1>
+	<p>Sorry, we're lost.</p>
 </section>
 
 <?php get_footer(); ?>
