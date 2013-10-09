856ec64fc74bab98b550fd437d24b247eabd2a26
Merlin Mason <info@merlinmason.co.uk>
2013-10-10 00:10:02 +0100

    shuffle DOM

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer class="page-footer"></footer>
-
 </div>
+
+<footer class="page-footer"></footer>
 
 <?php wp_footer(); ?>
 <script src="<?php bloginfo("template_url"); ?>/bower_components/parallax/source/jquery.parallax.js"></script>
