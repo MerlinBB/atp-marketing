2bc93991d191c990d40a1cb7935aba68b3f0a4bb
Merlin Mason <info@merlinmason.co.uk>
2013-10-12 12:39:18 +0100

    move container to template

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-<section class="fourohfour">
-	<h1>404 - Dang!</h1>
-	<p>Sorry, we're lost.</p>
-</section>
+<div class="container nopadding">
+	<section class="fourohfour">
+		<h1>404 - Dang!</h1>
+		<p>Sorry, we're lost.</p>
+	</section>
+</div>
 
 <?php get_footer(); ?>
