c42aa4d310d80f2d406cc12c83e218ed9c6ccb76
Merlin Mason <info@merlinmason.co.uk>
2013-10-16 18:15:27 +0100

    revert slideshow controls to normal positioning

--- _products.php
+++ _products.php
@@ -47,11 +47,6 @@
 									<img src="<?php the_field("highlight_feature_2_image"); ?>" alt="icon" class="scale img-circle mb-1x">
 									<h3 class="subheading"><?php the_field("highlight_feature_2_title"); ?></h3>
 									<?php the_field("highlight_feature_2_description"); ?>
-
-									<button class="moveslideshow slideshow-nav prev" data-destination="0">Back</button>
-									<?php if (get_field("third_slide")) { ?>
-										<button class="moveslideshow slideshow-nav next" data-destination="2">More</button>
-									<?php } ?>
 								</div>
 							</div>
 						</div>
@@ -69,6 +64,10 @@
 										<?php endwhile; ?>
 									</ul>
 								<?php endif; ?>
+								<button class="moveslideshow" data-destination="0">Back</button>
+								<?php if (get_field("third_slide")) { ?>
+									<button class="moveslideshow" data-destination="2">Read More</button>
+								<?php } ?>
 							</div>
 						</div>
 					</div>
@@ -90,7 +89,6 @@
 										<img src="<?php the_field("highlight_feature_4_image"); ?>" alt="icon" class="scale img-circle mb-1x">
 										<h3 class="subheading"><?php the_field("highlight_feature_4_title"); ?></h3>
 										<?php the_field("highlight_feature_4_description"); ?>
-										<button class="moveslideshow slideshow-nav prev" data-destination="1">Back</button>
 									</div>
 								</div>
 							</div>
@@ -108,6 +106,7 @@
 											<?php endwhile; ?>
 										</ul>
 									<?php endif; ?>
+									<button class="moveslideshow" data-destination="1">Back</button>
 								</div>
 							</div>
 						</div>
