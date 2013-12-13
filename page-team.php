37a84fb778155dbfeb1b948b424a575ce5a3f72c
Merlin Mason <info@merlinmason.co.uk>
2013-12-13 12:33:38 +0000

    build tem page

--- page-team.php
+++ page-team.php
@@ -1,12 +1,26 @@
 <?php get_header(); ?>
 
 <div class="page-title team">
-	<div class="container nopadding">
-		<h1>Who We Are</h1>
+	<div class="container">
+        <div class="row">
+    		<h1>Who We Are</h1>
+        </div>
 	</div>
 </div>
 
-<div class="container nopadding">
-	<h1>The Team</h1>
+<div class="container">
+
+    <?php if(get_field('team_members')): ?>
+        <ul class="team-members">
+            <?php while(has_sub_field('team_members')): ?>
+                <li>
+                    <img src="<?php the_sub_field('mug_shot'); ?>" alt="" class="scale img-circle mb-1x">
+                    <h3 class="subheading text-center"><?php the_sub_field('name'); ?></h3>
+                    <p class="text-justify"><?php the_sub_field('bio'); ?></p>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php endif; ?>
+
 </div>
 <?php get_footer(); ?>
